c4dacc3fef7e59d842807f6b407d37d40885e078
anti-dominator <akiyoshinakao95911@outlook.com>
2024-08-11 00:24:42 +0900

    change:  next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async headers() {
     return [
       {
@@ -9,7 +10,7 @@ module.exports = {
           { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "GET,DELETE,PATCH,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
@@ -21,3 +22,5 @@ module.exports = {
     ];
   },
 };
+
+export default nextConfig;
